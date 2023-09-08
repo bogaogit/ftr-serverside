b3250b4ab9ba7267a3bd9af45b5e5026353d8409
Bo Gao <gaobobuq@gmail.com>
2023-09-08 13:49:58 +1000

    serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StatsController } from './stats/stats.controller';
 import { UserInputController } from './user-input/user-input.controller';
+import { DEFAULT_ROOT_PATH, ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(DEFAULT_ROOT_PATH, '..', 'ftr-clientside'),
+    }),
+  ],
   controllers: [AppController, StatsController, UserInputController],
   providers: [AppService],
 })
